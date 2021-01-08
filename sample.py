4c85d02f424cd6b5ebe78be0f2c405770692a895
Ethan-Tsao <tsao.ethan33@gmail.com>
2021-01-07 23:55:56 -0600

    initialized some preliminary functions

---
+++ sample.py
@@ -0,0 +1,41 @@
+import sqlite3 as db
+import datetime as dt
+
+# produce a sample script first as a baseline to conver to a full application
+
+def init_db():
+    '''
+    initialize database with budget and expenses tables
+    '''
+    pass
+
+def init_budget():
+    '''
+    initialize new table to produce the budget if non-existant
+    - parameters should include monthly or yearly budget
+    - if monthly budget, interpolate into yearly budget
+    '''
+    pass
+
+def init_expenses():
+    '''
+    initialize table with fields: type (earning or expense), amount, category, note, date if non-existant
+    '''
+    pass
+
+def add_categories():
+    '''
+    add category or categories to budget and earnings/expenses tables
+    '''
+
+def log():
+    '''
+    add an entry to a earnings/expenses table
+    '''
+    pass
+
+
+
+
+if __name__ == "__main__":
+    pass
